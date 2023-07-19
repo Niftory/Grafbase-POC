4d0bc1a782a6847873514233bcba2ce98edb7cdd
Rojan Maharjan <roseonmhr@gmail.com>
2023-07-19 21:50:45 +0545

    fix: update artists

--- grafbase/resolvers/artists.ts
+++ grafbase/resolvers/artists.ts
@@ -1,7 +1,5 @@
 import { client } from "../client";
 
 export default async function artists(ctx, arg, what, info) {
-  return client.query.artist.findMany({
-    with: { Album: { with: { Track: true } } },
-  });
+  return client.query.artist.findMany({});
 }
